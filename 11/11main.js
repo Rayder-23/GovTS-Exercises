b14d3972c6149dc0ed3b14cd861618609a571a97
Rayder <165102003+Rayder-23@users.noreply.github.com>
2024-07-01 00:57:37 +0500

    added labels

--- 11/11main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let names = [
-    "Ali", "Zia", "Jeff"
-];
-// .forEach function/loop used
-names.forEach(friendName => console.log(friendName));
--- 13/13main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let cars = [
-    "Nissan GTR Skyline R34", "Mitsubishi Lancer Evo 4", "McLaren 720s"
-];
-cars.forEach(car => console.log(`I would like to own a ${car}.`));
